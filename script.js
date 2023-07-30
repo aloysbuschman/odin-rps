d277ac87213f464b853712923d6bbf4bba0a2c6e
aloysbuschman <aloys.buschman@gmail.com>
2023-07-30 15:49:01 +0200

    Add getComputerChoice()

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('test')
+function getComputerChoice() {
+    const options = ["Rock", "Paper", "Scissors"]
+    return options[Math.floor(Math.random() * 3)]
+}
+
+console.log(getComputerChoice())
+console.log(getComputerChoice())
+console.log(getComputerChoice())
+console.log(getComputerChoice())
+console.log(getComputerChoice())

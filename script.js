f8f6b994d232a1d97ed581eb2a974e7992f311ac
aloysbuschman <aloys.buschman@gmail.com>
2023-07-31 09:50:27 +0200

    Add playRound()

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
+let playerSelection = "rock";
+let playerScore = 0; 
+let computerScore = 0; 
+
 function getComputerChoice() {
-    const options = ["Rock", "Paper", "Scissors"]
+    const options = ["rock", "paper", "scissors"]
     return options[Math.floor(Math.random() * 3)]
 }
 
-console.log(getComputerChoice())
-console.log(getComputerChoice())
-console.log(getComputerChoice())
-console.log(getComputerChoice())
-console.log(getComputerChoice())
+function playRound(playerSelection) {
+    let computerSelection = getComputerChoice()
+    console.log(playerSelection, computerSelection)
+    if (playerSelection === computerSelection) {
+        return `It was a draw both parties selected ${playerSelection}`
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')
+
+    ) {
+        playerScore++ 
+    } else {
+        computerScore++
+    }
+}
+
+
+playRound(playerSelection)
+console.log({playerScore}, {computerScore})
